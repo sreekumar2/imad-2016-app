cd3f10caaab543edc3edf632ec5b84bf37699d25
sreekumar2 <srikmr42@gmail.com>
2016-09-24 15:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-one', function (req, res){
     res.sendfile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 app.get('/article-two', function (req, res){
-   res.sendfile(path.join(__dirname, 'ui', 'index.html')); 
+   res.sendfile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 app.get('/article-three', function (req, res){
-    res.sendfile(path.join(__dirname, 'ui', 'index.html')); 
+    res.sendfile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
 

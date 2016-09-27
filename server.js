32e6b3b38620e3392134d169dead234b1cc1ae85
sreekumar2 <srikmr42@gmail.com>
2016-09-27 20:41:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,14 @@ var htmlTemplate = `
                            ${title}
                         </title>
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
-                        <link href="/ui/style.css" rel="stylesheet" />
+                        .cointainer {
+    max-width: 800px;
+    margin: 0 auto;
+    color: #050596;
+    font-family: sans-serif;
+    padding-left: 20px;
+    padding-right: 20px;
+ }
                     </head>
                     <body>
                         <div class="cointainer">

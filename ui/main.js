40cd0256a77a656f306e86e2069ea51bdd5fe61e
sreekumar2 <srikmr42@gmail.com>
2016-09-28 16:52:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'Hello sree';
 
 //move the image
 var img = document.getElementById('madi'); 
-var marginLeft =0;
+var marginLeft = 0;
 function moveRight () {
     marginLeft - marginLeft + 10;
     img.style.marginLeft = marginLeft +'px';

ecc606d0e55d0dcf403120bfb90b2ebf44cf0091
sreekumar2 <srikmr42@gmail.com>
2016-09-28 15:13:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/:articleName', function (req, res){
    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/styles.css', function (req, res){
+app.get('/ui/style.css', function (req, res){
    res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
 

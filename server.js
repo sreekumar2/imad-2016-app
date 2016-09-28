d05811e2120e0caa8534c916d3d3bf85283a9e62
sreekumar2 <srikmr42@gmail.com>
2016-09-28 18:10:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,12 @@ var htmlTemplate = `
 app.get('/', function (req, res){
    res.sendfile(path.join(__dirname, 'ui', 'index.html')); 
 });
+
+var counter = 0;
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
     
 app.get('/:articleName', function (req, res){
     //articlenmae==article-one

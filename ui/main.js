070a108c24cd1505d31dfcc285921f11d45a1f35
sreekumar2 <srikmr42@gmail.com>
2016-09-28 16:11:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element  = document.getElementById('main-text'
 element.innerHTML = 'Hello sree';
 
 //move the image
-var img = document.getElementById('madi'); 
+var img = document.getElementById('madio'); 
 img.oneclick = function (){
   img.style.marginLeft = "100px";
  

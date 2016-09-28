065a47dadc2d92e9da9be4548d70abe0d3af4259
sreekumar2 <srikmr42@gmail.com>
2016-09-28 17:01:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ var element  = document.getElementById('main-text'
 element.innerHTML = 'Hello sree';
 
 //move the image
-var img = document.getElementById('madi'); 
-img.onclick = function () {
-   img.style.marginLeft = '100px';
- };
+var img = document.getElementById('madi');
+var margingLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+    }; 
